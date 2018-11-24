bcd74a948a05713a2906e07b7661c2d3c88ce00a
safu9 <safu9.dev@gmail.com>
2018-11-25 01:42:15 +0900

    Init chat app

---
+++ chat/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'chat/index.html'
