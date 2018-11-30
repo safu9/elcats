c203f2a832fa9315bad2842a40923d11094b2c3b
safu9 <safu9.dev@gmail.com>
2018-11-30 11:43:56 +0900

    Adjust form styles

--- home/templatetags/bulma.py
+++ home/templatetags/bulma.py
@@ -7,19 +7,13 @@ register = template.Library()
 
 
 @register.filter(needs_autoescape=True)
-def bulma_input(value, autoescape=True):
+def bulma_form(value, autoescape=True):
     if autoescape:
         value = conditional_escape(value)
 
     value = value.replace('input type="text" ', 'input type="text" class="input" ')
     value = value.replace('input type="date" ', 'input type="date" class="input" ')
+    value = value.replace('input type="password" ', 'input type="password" class="input" ')
+    value = value.replace('textarea ', 'textarea class="textarea" ')
 
     return mark_safe(value)
-
-
-@register.filter(needs_autoescape=True)
-def bulma_textarea(value, autoescape=True):
-    if autoescape:
-        value = conditional_escape(value)
-
-    return mark_safe(value.replace('textarea ', 'textarea class="textarea" '))
