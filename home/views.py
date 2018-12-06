30210a8257133f63d9d9c06eca56115f4310672b
safu9 <safu9.dev@gmail.com>
2018-12-06 12:23:17 +0900

    Change context_object_name of project

--- home/views.py
+++ home/views.py
@@ -29,11 +29,13 @@ class ProjectCreateView(LoginRequiredMixin, generic.CreateView):
 class ProjectDetailView(ProjectAccessMixin, generic.DetailView):
     template_name = 'home/project_detail.html'
     model = Project
+    context_object_name = 'project'
 
 
 class ProjectUpdateView(ProjectAccessMixin, generic.UpdateView):
     template_name = 'home/project_update.html'
     model = Project
+    context_object_name = 'project'
     form_class = ProjectForm
 
     def get_success_url(self):
@@ -43,4 +45,5 @@ class ProjectUpdateView(ProjectAccessMixin, generic.UpdateView):
 class ProjectDeleteView(ProjectAccessMixin, generic.DeleteView):
     template_name = 'home/project_delete.html'
     model = Project
+    context_object_name = 'project'
     success_url = reverse_lazy('home:project_list')
