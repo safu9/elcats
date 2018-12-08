535dfa283cbfa3086073dbb9373e1bad17574a1e
safu9 <safu9.dev@gmail.com>
2018-12-08 21:28:24 +0900

    Change field type of project description

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
 
     name = models.CharField('名前', max_length=50)
     slug = models.SlugField('スラッグ', max_length=50, unique=True)
-    description = models.TextField('説明', max_length=50, blank=True)
+    description = models.CharField('説明', max_length=50, blank=True)
     members = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name='メンバー')
     is_private = models.BooleanField('非公開', default=False)
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
